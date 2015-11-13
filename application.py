a9f1985f48ac414ca1c47ece0204b9f0a08afb15
mike <mikelewek@gmail.com>
2015-11-12 23:12:08 -0500

    feat: edit funtionality and templates added

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Item, Category
+import pprint
 
 app = Flask(__name__)
 
@@ -19,15 +20,16 @@ session = Session()
 def home():
     # im not sure where this should go
     # url_for('css', filename = 'style.css')
-    categories = session.query(Category).all()
-    items = session.query(Item).all()
+    categories = query('cats', False)
+    items = query('items', False)
     return render_template('index.html', cats=categories, items=items)
 
 
-# create category form
+# create category
 @app.route('/create-category', methods=['POST', 'GET'])
 def create_category():
-    message = 'no data for db insert'
+    message = 'No data for db insert'
+
     if request.method == 'POST':
         cat = Category(title=request.form['title'])
         session.add(cat)
@@ -36,10 +38,29 @@ def create_category():
     return render_template("create-category.html", message=message)
 
 
-# create item form
+# edit category
+@app.route('/edit-category', methods=['POST', 'GET'])
+def edit_category():
+    message = ''
+    cat = ''
+
+    if request.method == 'GET':
+        cat = query('cats', request.args.get('id'))
+
+    if request.method == 'POST':
+        cat = Category(title=request.form['title'])
+        session.add(cat)
+        session.commit()
+        message = 'Category updated successfully'
+    return render_template("edit-category.html",
+                           cat=cat,
+                           message=message)
+
+
+# create item
 @app.route('/create-item', methods=['POST', 'GET'])
 def create_item():
-    message = 'no data for db insert'
+    message = 'No data for db insert'
     cats = query('cats')
 
     if request.method == 'POST':
@@ -54,12 +75,45 @@ def create_item():
                            message=message)
 
 
+# edit item
+@app.route('/edit-item', methods=['POST', 'GET'])
+def edit_item():
+    cats = query('cats')
+    items = ''
+    message = ''
+
+    if request.method == 'GET':
+        items = query('items', request.args.get('id'))
+
+    if request.method == 'POST':
+        item = Item(title=request.form['title'],
+                    description=request.form['description'],
+                    category_id=request.form['category_id'])
+        session.add(item)
+        session.commit()
+        message = 'Item successfully updated'
+    return render_template("edit-item.html",
+                           items=items,
+                           categories=cats,
+                           message=message)
+
+
 # query db for items or categories
-def query(var):
-    if var == 'cats':
-        result = session.query(Category).all()
-    elif var == 'items':
-        result = session.query(Item).all()
+def query(table, qid=False):
+    result = ''
+
+    if table == 'cats':
+        if qid is not False:
+            result = session.query(Category).filter_by(id=qid)
+        else:
+            result = session.query(Category).all()
+
+    if table == 'items':
+        if qid is not False:
+            result = session.query(Item).filter_by(id=qid)
+        else:
+            result = session.query(Item).all()
+
     return result
 
 
