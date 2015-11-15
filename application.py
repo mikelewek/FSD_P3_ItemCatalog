e2e91f902e51dadcae2724a9208bbfa6a1c63036
mike <mikelewek@gmail.com>
2015-11-15 17:09:16 -0500

    test

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
+from requests_oauthlib import OAuth2Session
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Item, Category
-import pprint
+import os
 
 app = Flask(__name__)
 
@@ -15,12 +16,65 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+# This information is obtained upon registration of a new GitHub OAuth
+# application here: https://github.com/settings/applications/new
+client_id = "<your client key>"
+client_secret = "<your client secret>"
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+
+
+@app.route("/demo")
+def demo():
+    """Step 1: User Authorization.
+
+    Redirect the user/resource owner to the OAuth provider (i.e. Github)
+    using an URL with a few key OAuth parameters.
+    """
+    github = OAuth2Session(client_id)
+    authorization_url, state = github.authorization_url(authorization_base_url)
+
+    # State is used to prevent CSRF, keep this for later.
+    session['oauth_state'] = state
+    return redirect(authorization_url)
+
+
+# Step 2: User authorization, this happens on the provider.
+@app.route("/callback", methods=["GET"])
+def callback():
+    """ Step 3: Retrieving an access token.
+
+    The user has been redirected back from the provider to your registered
+    callback URL. With this redirection comes an authorization code included
+    in the redirect URL. We will use that to obtain an access token.
+    """
+
+    github = OAuth2Session(client_id, state=session['oauth_state'])
+    token = github.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.url)
+
+    # At this point you can fetch protected resources but lets save
+    # the token and show how this is done from a persisted token
+    # in /profile.
+    session['oauth_token'] = token
+
+    return redirect(url_for('.profile'))
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Fetching a protected resource using an OAuth 2 token.
+    """
+    github = OAuth2Session(client_id, token=session['oauth_token'])
+    return jsonify(github.get('https://api.github.com/user').json())
+
+
 # homepage - displays categories and associated items
 @app.route('/')
 def home():
     # get all category and items
     categories = session.query(Category).all()
-    items = session.query(Item).all()
+    items = session.query(Item).limit(10)
     return render_template('index.html',
                            cats=categories,
                            items=items)
@@ -170,3 +224,4 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+    app.secret_key = os.urandom(24)
